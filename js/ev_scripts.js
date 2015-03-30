c572ba9b44dab29fb34465ee4105a09466c917fc
identity-verification-services <identity-verification-services@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-30 11:31:43 +0000

    Jquery Changes
    git-svn-id: https://plugins.svn.wordpress.org/email-verification/trunk@1123568 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/ev_scripts.js
+++ js/ev_scripts.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function(){
 
 	var fullpath=window.location.href;
 	var split_path=fullpath.split("/");
-	if(split_path[4]=='' || split_path[4].match(/doing/g)){
+	if(split_path[4]==''){
 		var site_url='http://'+window.location.hostname;
 	}else{
 		var site_url='http://'+window.location.hostname+"/"+split_path[3];
