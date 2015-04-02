dac5a64430cdd325468f535c8b14dcb5d05fe939
identity-verification-services <identity-verification-services@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-02 09:13:25 +0000

    form and css changes
    git-svn-id: https://plugins.svn.wordpress.org/email-verification/trunk@1126148 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- verification_form.php
+++ verification_form.php
@@ -3,25 +3,37 @@
 <?php
 if(count($configuration_details)>0 && in_array  ('curl', get_loaded_extensions())){
 ?>
-<div class="ivs-form1">
-	<div class="ivs-message_ev alert-static"></div>
-	<div class="ivs-inner-form">
-		<div class="ivs-field">
-			<label for="driving_license">Email</label>
-			<div class="ivs-input">	
-				<input type="text" name="email" id="email" placeholder="Email to be Verified" style="width:70% !important;float:left;margin-left:5px" ><a  class="" href="#" id="verify_ev" >VERIFY</a>
-				<span class="err err_email"></span>
-			</div>		
-			<div class="clear"></div>
+
+<div class="main-form-plg email">
+	<div class="ivs-form">
+		<h5>Email Verification</h5>
+		<div class="ivs-message_ev alert-static"></div>
+		<div class="ivs-inner">
+			<div class="ivs-field-form">
+				
+				<div class="ivs-input">	
+					<input type="text" name="email" id="email" placeholder="Email to be Verified"  >
+					<span class="err err_email"></span>
+					<div class="text-right">
+						<button class="btn-ivs-form" id="verify_ev"> VERFY</button>  
+						
+					</div>		
+				</div>		
+				<div class="clear"></div>
+			</div>
+			
 		</div>
-		
-	</div>
-	<div class="resend-block" style="display:none">
-		<input type="hidden" id="sent_email">
-		Still waiting for your Verification Link? Click <a href="#?" class="resend_email">RESEND</a><br/><br/>
-		
+		<div class="text-center">
+			<div class="resend-block" style="display:none">
+				<input type="hidden" id="sent_email">
+				Still waiting for your Verification Link? Click 
+				<button class="resend_email" >RESEND</button>
+				
+			</div>
+		</div>
 	</div>
 </div>
+<div class="clear"></div>
 <?php
 }else{
 ?>
